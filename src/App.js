00ccd57e9b83df2dc3ee673a99afdfa2baab8309
deepak <d1620k@yahoo.com>
2023-08-02 22:24:54 +0530

    initial commmit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import Header from './components/header';
 import { Outlet } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { setProductData } from './redux/productSlice';
 import { useDispatch } from 'react-redux';
 
 function App() {
   const dispatch = useDispatch()
+  const [data,setData ] = useState()
   // const allProducts = useSelector((state) => state.product)
-  useEffect(() => {
-    async function fetchProduct(){
 
+  async function fetchProduct(){
     const Server = process.env.REACT_APP_SERVER_DOMAIN ? process.env.REACT_APP_SERVER_DOMAIN : "https://groceries-yipj.onrender.com"
     const res = await fetch(`${Server}/product`, {
       method: "GET",
@@ -20,10 +20,12 @@ function App() {
       },
     })
     const resData = await res.json()
+    setData(data)
     dispatch(setProductData(resData))
   }
+  useEffect(() => {
    fetchProduct()
-  }, [])
+  }, [""])
   
 
   return (
