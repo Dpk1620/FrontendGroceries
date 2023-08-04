8019853816241d1492937ba766107effe50c6184
deepak <d1620k@yahoo.com>
2023-08-04 15:56:28 +0530

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({ name, image, category, price, loading, id }) => {
                     <>
                         <Link to={`/menu/${id}`} onClick={() => window.scrollTo({ top: "0", behavior: "smooth" })} >
                             <div className="w-40 min-h-[150px]">
-                                <img src={image} className="h-full w-full" />
+                                <img src={image} className="h-full w-full" alt='' />
                             </div>
                             <h3 className="font-semibold text-slate-600 text-center capitalize text-lg">
                                 {name}
